4df2566f69aad09f97050de6ef7fdd3c6d3f09e6
Nguyen Khoi <vanngelttt@gmail.com>
2025-04-13 22:44:51 +0700

    Init repository

---
+++ Flutter_3.dart
@@ -0,0 +1,112 @@
+// Abstract class Employee
+abstract class Employee {
+  final String id;
+  final String name;
+
+  Employee(this.id, this.name);
+
+  void printInfo() {
+    print('Mã số nhân viên: $id, Tên nhân viên: $name');
+  }
+
+  double calculateSalary();
+}
+
+abstract class Workable {
+  void doWork();
+}
+
+class OfficeWorker extends Employee implements Workable {
+  final String department;
+  final double baseSalary;
+  final double bonus = 1.2;
+
+  OfficeWorker(String id, String name, this.department, this.baseSalary)
+    : super(id, name);
+
+  @override
+  void printInfo() {
+    super.printInfo();
+    print('Phòng ban làm việc: $department');
+  }
+
+  @override
+  double calculateSalary() {
+    return baseSalary * this.bonus;
+  }
+
+  @override
+  void doWork() {
+    print('Nhân viên $name đang làm việc tại phòng $department');
+  }
+}
+
+class TechWorker extends Employee implements Workable {
+  final String field;
+  final double baseSalary;
+  final double bonus = 1.5;
+
+  TechWorker(String id, String name, this.field, this.baseSalary)
+    : super(id, name);
+
+  @override
+  void printInfo() {
+    super.printInfo();
+    print('Lĩnh vực kỹ thuật: $field');
+  }
+
+  @override
+  double calculateSalary() {
+    return baseSalary * this.bonus;
+  }
+
+  @override
+  void doWork() {
+    print('$name đang làm việc trong lĩnh vực $field');
+  }
+}
+
+class Company {
+  List<Employee> employees = [];
+
+  void addEmployee(Employee employee) {
+    employees.add(employee);
+  }
+
+  void printAllEmployees() {
+    print('DANH SÁCH NHÂN VIÊN CỦA CÔNG TY:');
+    for (var employee in employees) {
+      employee.printInfo();
+      print('----------------------------');
+    }
+  }
+
+  // Tính tổng lương của công ty
+  double getTotalSalary() {
+    double total = 0;
+    for (var employee in employees) {
+      total += employee.calculateSalary();
+    }
+    return total;
+  }
+}
+
+void main() {
+  var company = Company();
+
+  company.addEmployee(
+    OfficeWorker('OW001', 'Nguyễn Văn A', 'Kế toán', 10000000),
+  );
+  company.addEmployee(OfficeWorker('OW002', 'Trần Thị B', 'Nhân sự', 12000000));
+
+  company.addEmployee(
+    TechWorker('TW001', 'Lê Văn C', 'Phát triển phần mềm', 15000000),
+  );
+  company.addEmployee(
+    TechWorker('TW002', 'Phạm Thị D', 'Quản trị hệ thống', 14000000),
+  );
+
+  company.printAllEmployees();
+
+  print('Tổng lương của công ty: ${company.getTotalSalary()} VND');
+}
